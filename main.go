363e1092a1d247dae4c3a3a50baba0c9f291ac64
Fábio Gomes <fabioxgn@gmail.com>
2015-09-23 23:48:12 -0300

    Changed to the new go-bot package location

--- main.go
+++ main.go
@@ -3,14 +3,14 @@ package main
 import (
 	"os"
 
-	"github.com/fabioxgn/go-bot"
-	_ "github.com/fabioxgn/go-bot/commands/catfacts"
-	_ "github.com/fabioxgn/go-bot/commands/catgif"
-	_ "github.com/fabioxgn/go-bot/commands/chucknorris"
+	"github.com/go-chat-bot/bot/slack"
+	_ "github.com/go-chat-bot/plugins/catfacts"
+	_ "github.com/go-chat-bot/plugins/catgif"
+	_ "github.com/go-chat-bot/plugins/chucknorris"
 
 	// Import all the commands you wish to use
 )
 
 func main() {
-	bot.RunSlack(os.Getenv("SLACK_TOKEN"))
+	slack.RunSlack(os.Getenv("SLACK_TOKEN"))
 }
